2fec62ef2dc24a2cf54c4da21d8ac8ec80685df8
Thedas Sri Harisha <99543091+it21114830@users.noreply.github.com>
2022-03-05 10:48:40 +0000

    tutorial 02 - it21114830

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,42 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(void)
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
+	
+	double salary,netSalary;
+	int etype, otHrs ,otRate;
+	
+	cout<<"Enter Employee Type :";
+	cin>>salary;
 
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
-   return 0;
+  cout<<"Enter Salary : ";
+  cin>>salary;
+	
+	cout<<"Enter others :";
+	cin>>otHrs;
+	
+	switch(etype)
+    {
+    	case 1:
+    		   otRate = 1000;
+    		   break;
+    	case 2:
+		       otRate = 1500;
+			   break;
+		default:
+		       otRate = 1700;
+			   break;	   	   
+	}
+	
+	netSalary=salary + otHrs *otRate;
+	
+	cout<<"Net salary is "<<setiosflags(ios::fixed)<<setprecision(2)<<netSalary<<endl;
+	
+	return 0;	
 }
--- tute04.cpp
+++ tute04.cpp
@@ -12,7 +12,7 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
-
+#include <math.h>
 long Factorial(int no);
 long nCr(int n, int r);
 
@@ -22,8 +22,21 @@ int main() {
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
+  
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
-  return 0;
+   return 0;
+}
+long Factorial(int no){
+  fac = 1;
+    for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+    }
+  return fac;
+}
+
+long nCr(int n, int r)
+{
+  return n!/ r!(n−r)! ;
 }
